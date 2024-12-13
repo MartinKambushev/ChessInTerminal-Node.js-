77d7fb83888b5386a13abcbfa21fa1ff24a43e68
MartinKambushev <72106333+MartinKambushev@users.noreply.github.com>
2024-12-13 17:33:41 +0200

    Update chess.js
    Addet validation for the movement of pawn and rook

--- chessGameJS/chess.js
+++ chessGameJS/chess.js
@@ -1,4 +1,5 @@
 const readline = require('readline')
+const { isWeakMap } = require('util/types')
 let board = [
     ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
     ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
@@ -30,8 +31,55 @@ function movePiece(board, from, to){
 }
 function isValidMove(board, from, to, currentP){
     const piece = board[from.row][from.col]
+    const target = board[to.row][to.col]
+
     if (piece === ' ') return false;
     return true
+    const isWhite = piece === piece.toUpperCase()
+    if(currentP === 'white' && !isWhite) return false
+    if(currentP === 'black' && isWhite)return false
+    if(to.row < 0 || to.row > 7 || to.col < 0 || to.col > 7) return false
+    if(target !== ' ' && isWhite === (target === terget.toUpperCase())) return false
+    switch( piece.toLowerCase()){
+        case 'p': return validatePawnMove(board, from, to, isWhite)
+        case 'r': return validateRookMove(board,from, to)
+        case 'n': return validateKnightMove(from,to)
+        case 'b': return validateBishopMove(board, from, to)
+        case 'q': return validateQueenMove(board,from, to)
+        case 'k': return validateKingMove(from, to)
+        default: return false
+    }
+}
+function validatePawnMove(board, from,to){
+    const direction = isWhite ? -1 : 1
+    const startRow = isWhite ? 6 : 1
+    //Напред
+    if (to.col === from.col && board[to.row][to.col] === ' '){
+        if (to.row === from.row + direction) return true;
+    if (to.row === from.row + 2 * direction && from.row === startRow && board[from.row+direction][from.col] === ' ') return true
+    }
+    //Взимане
+    if(Math.abs(to.col - from.col)=== 1 && to.row === from.row + direction && board[to.row][to.col] !== ' '){
+        return true
+    }
+    return false
+}
+function validateRookMove(board,from, to){
+    if(from.row === to.row){
+        const step = from.col < to.col ? 1:-1
+        for(let col = from.col+step; col!==to.col; col +=step){
+            if(board[from.row][col] !== ' ')return false
+        }
+
+    }
+    else if (from.col === to.col){
+        const step = from.row < to.row ? 1:-1
+        for (let row = from.row + step; row!==to.row; row+=step){
+            if(board[row][from.col] !== ' ') return false
+        }
+        return true
+    }
+    return false
 }
 function playTurn(board, move){
     const {from, to} = parseMove(move)
